2325111135350b01000e2c6bf02d9a06f27fca43
gauri mahalle <gauri.mahalle@softxsolution.com>
2023-08-03 10:39:53 +0530

    Fix: thumbnail relaed issue

--- src/components/Thumbnail.jsx
+++ src/components/Thumbnail.jsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import { forwardRef } from "react";
 
-const Thumbnail = forwardRef(({ result }, ref) => {
+const Thumbnail = forwardRef(function Thumbnail({ result }, ref) {
   const BASE_URL = "https://image.tmdb.org/t/p/original";
   return (
     <div
@@ -16,6 +16,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
         }
         height={1080}
         width={1920}
+        alt="poster"
       />
       <div className="p-2">
         <p className="truncate max-w-md">{result?.overview}</p>
